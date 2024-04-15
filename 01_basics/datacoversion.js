67ac7696d17e046f67b8c6772bfc774cacc497dc
Akshat <70104707+Aksh1176@users.noreply.github.com>
2024-04-15 08:23:10 +0000

    datacoversion

---
+++ 01_basics/datacoversion.js
@@ -0,0 +1,19 @@
+let age = undefined;
+let convertedValue = Number(age);
+
+console.log(convertedValue);
+
+
+
+
+console.log(typeof convertedValue);
+
+/*
+
+"33" => 33
+"33abc" => NaN
+null => 0
+true => 1 , false => 0
+undefined => NaN
+
+*/
